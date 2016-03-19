aa1cfc72083acc8e753c7670db85e89a1f604e7a
Sim Kern Cheh <kern.cheh@gmail.com>
2016-03-19 20:53:21 +0800

    Test against git current branch

--- lib/base_branch/git_branch.rb
+++ lib/base_branch/git_branch.rb
@@ -1,18 +1,22 @@
 class BaseBranch::GitBranch
   class << self
     def current_branch
-      branches = `git branch -a`.split("\n")
-
       raise(
         BaseBranch::Exceptions::UninitializedRepository,
         'Please initialize a git repository and have at least one branch.'
-      ) if branches.empty?
+      ) if (branchez = branches).empty?
 
-      (branches.detect { |br| br[0, 2] == '* ' } || '').gsub('* ', '').chomp
+      (branchez.detect { |br| br[0, 2] == '* ' } || '').gsub('* ', '').chomp
     end
 
     def master?
       current_branch == 'master'
     end
+
+    def branches
+      `git branch -a`.split("\n")
+    end
   end
+
+  private_class_method :branches
 end
--- test/base_branch/git_branch_test.rb
+++ test/base_branch/git_branch_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class GitBranchTest < Minitest::Test
   def test_current_branch
-
+    BaseBranch::GitBranch.stub :branches, ['  branch2', '* master', '  remotes/origin/master'] do
+      assert_equal 'master', BaseBranch::GitBranch.current_branch
+    end
   end
 end
